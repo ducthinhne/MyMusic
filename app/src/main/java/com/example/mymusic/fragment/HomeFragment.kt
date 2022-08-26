93f8dbbcc192973832069923df6ec59117ac67da
NDThinhahihi <94670286+NDThinhahihi@users.noreply.github.com>
2022-08-26 10:07:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mymusic/fragment/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.example.mymusic.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mymusic.R
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.frament_home, container, false)
+    }
+}
